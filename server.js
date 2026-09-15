86df60c347eb0c696ae0fc03225987d23bcb7baf
mortezarajabzadeh700-max <mortezarajabzadeh700@gmail.com>
2026-09-16 00:11:11 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("سامانه آزمون مرتضی رجب زاده فعال است 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
